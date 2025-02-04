acd01bcf18a49ea413edbd9dded8c70d62fb6c99
Joan Amengual <joan@consilientlabs.com>
2025-02-04 13:45:48 +0100

    Modificaciones 2025

--- ec2-fundamentals/ec2-user-data.sh
+++ ec2-fundamentals/ec2-user-data.sh
@@ -5,4 +5,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Hola Mundo desde $(hostname -f)</h1>" > /var/www/html/index.html
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
